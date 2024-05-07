a42434b720467704f06da77173cc81f687b70fff
m-cvvirko <marek@cvvirko.pl>
2024-05-07 12:21:26 +0200

    added configs plus ignore file

---
+++ src/main/java/pl/achp/sinktools/config/KafkaProducerConfig.java
@@ -0,0 +1,16 @@
+package pl.achp.sinktools.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import pl.achp.sinktools.model.Message;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Bean
+    public KafkaTemplate<String, Message> kafkaTemplate(ProducerFactory<String, Message> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+}
