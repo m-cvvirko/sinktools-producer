925bf6e60611a4330823dfc558c52451c3e0d2bd
m-cvvirko <marek@cvvirko.pl>
2024-05-07 17:40:08 +0200

    added logging

--- src/main/java/pl/achp/sinktools/services/MessageService.java
+++ src/main/java/pl/achp/sinktools/services/MessageService.java
@@ -1,7 +1,8 @@
 package pl.achp.sinktools.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import pl.achp.sinktools.model.Message;
@@ -10,17 +11,17 @@ import pl.achp.sinktools.config.KafkaProperties;
 @Service
 public class MessageService {
 
+    private static final Logger logger = LoggerFactory.getLogger(MessageService.class);
+
     @Autowired
     private KafkaTemplate<String, Message> kafkaTemplate;
 
-//    @Value("${achp.kafka.topic.name}")
-//    private String topicName;
 
     @Autowired
     private KafkaProperties kafkaProperties;
 
     public void sendMessage(Message message) {
-        System.out.println("Sending message: " + kafkaProperties.getTopicName() + " | " + message);
-        kafkaTemplate.send(kafkaProperties.getTopicName(), message);
+        logger.info("Sending message: {} | {}", kafkaProperties.getTopicName(), message);
+//        kafkaTemplate.send(kafkaProperties.getTopicName(), message);
     }
 }
